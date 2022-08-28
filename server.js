c52c481f68fbb72180c03df9f04543e65aa9b84d
Rimuru <koonmilemkcc@gmail.com>
2022-08-28 04:53:42 +0000

    add branch dev

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดี11222211</h1>')
+  return res.send('<h1>สวัสดี11</h1>')
 })
 
 const PORT = process.env.PORT || 3000
